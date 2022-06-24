4a739db361330421e379d33365397e1f168802d9
Denis Ryabchenko <denisryabchenko@gmail.com>
2022-06-24 16:20:19 +0200

    udpate

--- src/App.js
+++ src/App.js
@@ -4,8 +4,7 @@ import React, { useEffect, useState, useCallback } from 'react';
 import DailyIframe from '@daily-co/daily-js';
 import { DailyProvider } from '@daily-co/daily-react-hooks';
 
-import api from './api';
-import { roomUrlFromPageUrl, pageUrlFromRoomUrl } from './utils';
+import { pageUrlFromRoomUrl } from './utils';
 
 import HomeScreen from './components/HomeScreen/HomeScreen';
 import Call from './components/Call/Call';
@@ -15,11 +14,9 @@ import HairCheck from './components/HairCheck/HairCheck';
 import RoomsPanel from './components/RoomsPanel/RoomsPanel';
 import ParticipantsPanel from './components/ParticipantsPanel/ParticipantsPanel';
 
-import { ROOMS } from './constants';
-
 /* We decide what UI to show to users based on the state of the app, which is dependent on the state of the call object. */
 const STATE_IDLE = 'STATE_IDLE';
-const STATE_CREATING = 'STATE_CREATING';
+// const STATE_CREATING = 'STATE_CREATING';
 const STATE_JOINING = 'STATE_JOINING';
 const STATE_JOINED = 'STATE_JOINED';
 const STATE_LEAVING = 'STATE_LEAVING';
@@ -30,38 +27,19 @@ export default function App() {
   const [appState, setAppState] = useState(STATE_IDLE);
   const [roomUrl, setRoomUrl] = useState(null);
   const [callObject, setCallObject] = useState(null);
-  const [apiError, setApiError] = useState(false);
-
-  /**
-   * Create a new call room. This function will return the newly created room URL.
-   * We'll need this URL when pre-authorizing (https://docs.daily.co/reference/rn-daily-js/instance-methods/pre-auth)
-   * or joining (https://docs.daily.co/reference/rn-daily-js/instance-methods/join) a call.
-   */
-  const createCall = useCallback(() => {
-    setAppState(STATE_CREATING);
-    return api
-      .createRoom()
-      .then((room) => room.url)
-      .catch((error) => {
-        console.error('Error creating room', error);
-        setRoomUrl(null);
-        setAppState(STATE_IDLE);
-        setApiError(true);
-      });
-  }, []);
 
   /**
    * We've created a room, so let's start the hair check. We won't be joining the call yet.
    */
-  const startHairCheck = useCallback(async (url, force) => {
+  const startHairCheck = useCallback((url, force) => {
     const newCallObject = DailyIframe.createCallObject();
-    setRoomUrl(url);
     setCallObject(newCallObject);
+    setRoomUrl(url);
     if (force) {
       newCallObject.join({ url: url });
     } else {
       setAppState(STATE_HAIRCHECK);
-      await newCallObject.startCamera();
+      newCallObject.startCamera();
     }
   }, []);
 
@@ -88,15 +66,6 @@ export default function App() {
     await callObject.leave();
     callObject.join({ url: url });
   }, [callObject]);
-
-  /**
-   * If a room's already specified in the page's URL when the component mounts,
-   * join the room.
-   */
-  useEffect(() => {
-    const url = roomUrlFromPageUrl();
-    startHairCheck(url || ROOMS[0].url);
-  }, [startHairCheck]);
 
   /**
    * Update the page's URL to reflect the active call when roomUrl changes.
@@ -126,11 +95,6 @@ export default function App() {
           setAppState(STATE_JOINED);
           break;
         case 'left-meeting':
-          // callObject.destroy().then(() => {
-          // setRoomUrl(null);
-          // setCallObject(null);
-          // setAppState(STATE_IDLE);
-          // });
           break;
         case 'error':
           setAppState(STATE_ERROR);
@@ -165,31 +129,13 @@ export default function App() {
    * Show the call UI if we're either joining, already joined, or have encountered
    * an error that is _not_ a room API error.
    */
-  const showCall =
-    !apiError && [STATE_JOINING, STATE_JOINED, STATE_ERROR].includes(appState);
+  const showCall = [STATE_JOINING, STATE_JOINED, STATE_ERROR].includes(appState);
 
   /* When there's no problems creating the room and startHairCheck() has been successfully called,
   * we can show the hair check UI.*/
-  const showHairCheck = !apiError && appState === STATE_HAIRCHECK;
+  const showHairCheck = appState === STATE_HAIRCHECK;
 
   const renderApp = () => {
-    // If something goes wrong with creating the room.
-    if (apiError) {
-      return (
-        <div className="api-error">
-          <h1>Error</h1>
-          <p>
-            Room could not be created. Please check your local configuration in
-            `api.js`. For more information, check out the{' '}
-            <a href="https://github.com/daily-demos/call-object-react-daily-hooks/blob/main/README.md">
-              readme
-            </a>{' '}
-            :)
-          </p>
-        </div>
-      );
-    }
-
     // No API errors? Let's check our hair then.
     if (showHairCheck || showCall) {
       return (
@@ -197,7 +143,7 @@ export default function App() {
           {showHairCheck ? (
             <HairCheck joinCall={joinCall} cancelCall={startLeavingCall} />
           ) : (
-            <DailyProvider callObject={callObject}>
+            <>
               <div className='call-wrapper'>
                 <RoomsPanel
                   roomUrl={roomUrl}
@@ -207,7 +153,7 @@ export default function App() {
                 <ParticipantsPanel />
               </div>
               <Tray leaveCall={startLeavingCall} />
-            </DailyProvider>
+            </>
           )}
         </DailyProvider>
       );
@@ -215,7 +161,7 @@ export default function App() {
 
     // The default view is the HomeScreen, from where we start the demo.
     return (
-      <HomeScreen createCall={createCall} startHairCheck={startHairCheck} />
+      <HomeScreen startHairCheck={startHairCheck} />
     );
   };
 
--- src/components/HomeScreen/HomeScreen.js
+++ src/components/HomeScreen/HomeScreen.js
@@ -1,27 +1,18 @@
 import React from 'react';
 import './HomeScreen.css';
+import { ROOMS } from '../../constants';
 
-const onlyLoading = true;
-
-export default function HomeScreen({ createCall, startHairCheck }) {
+export default function HomeScreen({ startHairCheck }) {
   const startDemo = () => {
-    createCall().then((url) => {
-      startHairCheck(url);
-    });
+    startHairCheck(ROOMS[0].url);
   };
 
   return (
     <div className="home-screen">
-      {onlyLoading ? (
-        <p>Loading...</p>
-      ) : (
-        <>
-          <h1>React Daily Hooks custom video app</h1>
-          <p>Start the demo with a new unique room by clicking the button below.</p>
-          <button onClick={startDemo}>Click to start a call</button>
-          <p className="small">Select “Allow” to use your camera and mic for this call if prompted</p>
-        </>
-      )}
+      <h1>React Daily Hooks custom video app</h1>
+      <p>Start the demo with a new unique room by clicking the button below.</p>
+      <button onClick={startDemo}>Click to start a call</button>
+      <p className="small">Select “Allow” to use your camera and mic for this call if prompted</p>
     </div >
   );
 }
