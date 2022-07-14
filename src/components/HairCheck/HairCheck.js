34f05d278216533290a4f5e6ab512795f5262cd4
Majid Akbari <majid.akbari@outlook.com>
2022-07-14 10:47:33 -0400

    Added mic/cam toggle to haircheck

--- src/components/HairCheck/HairCheck.js
+++ src/components/HairCheck/HairCheck.js
@@ -5,20 +5,33 @@ import {
   useDevices,
   useDaily,
   useDailyEvent,
+  useAudioTrack,
 } from '@daily-co/daily-react-hooks';
 import UserMediaError from '../UserMediaError/UserMediaError';
 
 import './HairCheck.css';
 import { ROOMS } from '../../constants';
+import { CameraOff, CameraOn, MicrophoneOff, MicrophoneOn } from '../Tray/Icons';
 
 export default function HairCheck({ joinCall, cancelCall, roomUrl }) {
   const localParticipant = useLocalParticipant();
   const videoTrack = useVideoTrack(localParticipant?.session_id);
+  const audioTrack = useAudioTrack(localParticipant?.session_id);
+  const mutedVideo = videoTrack.isOff;
+  const mutedAudio = audioTrack.isOff;
   const { microphones, speakers, cameras, setMicrophone, setCamera, setSpeaker } = useDevices();
   const callObject = useDaily();
   const videoElement = useRef();
 
   const [getUserMediaError, setGetUserMediaError] = useState(false);
+
+  const toggleVideo = useCallback(() => {
+    callObject.setLocalVideo(mutedVideo);
+  }, [callObject, mutedVideo]);
+
+  const toggleAudio = useCallback(() => {
+    callObject.setLocalAudio(mutedAudio);
+  }, [callObject, mutedAudio]);
 
   useDailyEvent(
     'camera-error',
@@ -70,7 +83,7 @@ export default function HairCheck({ joinCall, cancelCall, roomUrl }) {
       {getUserMediaError ? (
         <UserMediaError />
       ) : (
-        <form className="hair-check" onSubmit={join}>
+        <div className="hair-check">
           <h1>Setup your hardware</h1>
           {/*Video preview*/}
           {videoTrack?.persistentTrack && <video autoPlay muted playsInline ref={videoElement} />}
@@ -87,16 +100,38 @@ export default function HairCheck({ joinCall, cancelCall, roomUrl }) {
             />
           </div>
 
+          {/*Camera select*/}
+          <div>
+            <label htmlFor="cameraOptions">Camera:</label>
+            <div className='input-container'>
+              <div className='icon-button' onClick={toggleVideo}>
+                {mutedVideo ? <CameraOff /> : <CameraOn />}
+              </div>
+              <select name="cameraOptions" id="cameraSelect" onChange={updateCamera} value={cameras?.find(c => c.selected)?.device?.deviceId}>
+                {cameras?.map((camera) => (
+                  <option key={`cam-${camera.device.deviceId}`} value={camera.device.deviceId}>
+                    {camera.device.label}
+                  </option>
+                ))}
+              </select>
+            </div>
+          </div>
+
           {/*Microphone select*/}
           <div>
             <label htmlFor="micOptions">Microphone:</label>
-            <select name="micOptions" id="micSelect" onChange={updateMicrophone} value={microphones?.find(c => c.selected)?.device?.deviceId}>
-              {microphones?.map((mic) => (
-                <option key={`mic-${mic.device.deviceId}`} value={mic.device.deviceId}>
-                  {mic.device.label}
-                </option>
-              ))}
-            </select>
+            <div className='input-container'>
+              <div className='icon-button' onClick={toggleAudio}>
+                {mutedAudio ? <MicrophoneOff /> : <MicrophoneOn />}
+              </div>
+              <select name="micOptions" id="micSelect" onChange={updateMicrophone} value={microphones?.find(c => c.selected)?.device?.deviceId}>
+                {microphones?.map((mic) => (
+                  <option key={`mic-${mic.device.deviceId}`} value={mic.device.deviceId}>
+                    {mic.device.label}
+                  </option>
+                ))}
+              </select>
+            </div>
           </div>
 
           {/*Speakers select*/}
@@ -111,25 +146,13 @@ export default function HairCheck({ joinCall, cancelCall, roomUrl }) {
             </select>
           </div>
 
-          {/*Camera select*/}
-          <div>
-            <label htmlFor="cameraOptions">Camera:</label>
-            <select name="cameraOptions" id="cameraSelect" onChange={updateCamera} value={cameras?.find(c => c.selected)?.device?.deviceId}>
-              {cameras?.map((camera) => (
-                <option key={`cam-${camera.device.deviceId}`} value={camera.device.deviceId}>
-                  {camera.device.label}
-                </option>
-              ))}
-            </select>
-          </div>
-
           <button onClick={join} type="submit">
             Join call
           </button>
           <button onClick={cancelCall} className="cancel-call">
             Back to start
           </button>
-        </form>
+        </div>
       )}
     </>
   );
